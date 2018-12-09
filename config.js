85c7b5ae7638cc50ec8de125166e571534feae4b
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2018-12-09 12:29:18 -0400

    Desplegando a Heroku con Mlab

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ process.env.NUMBER_FROM_TWILIO = "+13016846880"
 if (process.env.NODE_ENV === 'dev') {
   process.env.URL_DB = 'mongodb://localhost:27017/crm-control-diamante2'
 } else {
-  process.env.URL_DB = 'mongodb://edteam:edteam2018@ds235418.mlab.com:35418/edmaratones'
+  process.env.URL_DB = 'mongodb://CTolenk:CRMControlDiamante2018!@ds223760.mlab.com:23760/crm-control-diamante'
 }
